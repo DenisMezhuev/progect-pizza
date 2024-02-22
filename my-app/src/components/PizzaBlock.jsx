4354d3d5fece22f78a4160ff2b225a589c238a5e
Denis Ermakov <alexmezhuev@yandex.by>
2024-02-22 18:31:36 +0300

    add state buttom components PizzaBlock

--- my-app/src/components/PizzaBlock.jsx
+++ my-app/src/components/PizzaBlock.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PizzaBlock() {
+function PizzaBlock({ title, price }) {
+  const [pizzaCount, setPizzaCount] = useState(0);
   return (
     <div>
       {" "}
@@ -10,7 +11,7 @@ function PizzaBlock() {
           src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
           alt="Pizza"
         />
-        <h4 className="pizza-block__title">Чизбургер-пицца</h4>
+        <h4 className="pizza-block__title">{title}</h4>
         <div className="pizza-block__selector">
           <ul>
             <li className="active">тонкое</li>
@@ -23,8 +24,11 @@ function PizzaBlock() {
           </ul>
         </div>
         <div className="pizza-block__bottom">
-          <div className="pizza-block__price">от 395 ₽</div>
-          <div className="button button--outline button--add">
+          <div className="pizza-block__price">от {price} ₽</div>
+          <button
+            className="button button--outline button--add"
+            onClick={() => setPizzaCount((pizzaCount) => pizzaCount + 1)}
+          >
             <svg
               width="12"
               height="12"
@@ -38,8 +42,8 @@ function PizzaBlock() {
               />
             </svg>
             <span>Добавить</span>
-            <i>2</i>
-          </div>
+            <i>{pizzaCount}</i>
+          </button>
         </div>
       </div>
     </div>
