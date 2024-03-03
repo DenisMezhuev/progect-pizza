f9187635224520ce6fd846b1ce7edf37e914fdd2
Denis Ermakov <alexmezhuev@yandex.by>
2024-03-03 10:36:53 +0300

    add params

--- my-app/src/pages/FullPizza.jsx
+++ my-app/src/pages/FullPizza.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 function FullPizza() {
   const { id } = useParams();
   const [pizza, setPizza] = useState();
+  const navigate = useNavigate();
   useEffect(() => {
     async function fetchPizza() {
       try {
@@ -27,6 +28,9 @@ function FullPizza() {
       <img src={pizza.imageUrl} alt="Pizza" />
       <h2>{pizza.title}</h2>
       <h4>{pizza.price} ₽</h4>
+      <button onClick={() => navigate(-1)} className="button button--black">
+        <span>Вернуться назад</span>
+      </button>
     </div>
   );
 }
