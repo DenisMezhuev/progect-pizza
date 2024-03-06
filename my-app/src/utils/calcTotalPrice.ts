9097d1547d7fa610ea8d20200df402ea032479fd
Denis Ermakov <alexmezhuev@yandex.by>
2024-03-06 12:07:00 +0300

    Updates

--- my-app/src/utils/calcTotalPrice.ts
+++
@@ -1,8 +0,0 @@
-
-import { TCartItem } from "../components/redux/slices/cartSlice";
-
-const calcTotalPrice = (items: TCartItem[]) => {
-  return items.reduce((sum, elem) => (sum += elem.price * elem.count), 0);
-};
-
-export default calcTotalPrice;
